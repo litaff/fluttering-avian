66dc982e6fbbe7012aa50facaea5bf54af004ff6
litaff <a.gazda2k18@gmail.com>
2024-07-20 12:48:17 +0200

    Implemented basic obstacles

---
+++ fluttering-avian/scripts/obstacles/ObstacleManager.cs
@@ -0,0 +1,74 @@
+namespace fluttering_avian.obstacles;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Godot;
+
+public partial class ObstacleManager : Node
+{
+    [Export]
+    private Godot.Collections.Dictionary<ObstacleType, PackedScene> obstacles;
+    [Export]
+    private float spawnOffset;
+    [Export]
+    private float obstacleMoveSpeed;
+    [Export]
+    private Timer spawnTimer;
+    
+    private ObstacleSpawner spawner;
+    private List<Obstacle> activeObstacles;
+
+    public override void _Ready()
+    {
+        activeObstacles = new List<Obstacle>();
+        spawner = new ObstacleSpawner(
+            new Dictionary<ObstacleType, PackedScene>(obstacles));
+        spawnTimer.Timeout += SpawnObstacle;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        TranslateObstacles(delta);
+    }
+
+    private void SpawnObstacle()
+    {
+        // TODO: Implement logic to swap between different obstacles.
+        SpawnObstacle(ObstacleType.Regular);
+    }
+    
+    private void SpawnObstacle(ObstacleType type)
+    {
+        var obstacle = spawner.GetObstacle(type);
+        var initialPosition = new Vector3(0, obstacle.GetRandomSpawnHeight(), spawnOffset);
+        obstacle.Position = initialPosition;
+        activeObstacles.Add(obstacle);
+        obstacle.RegisterHandlers();
+        AddChild(obstacle);
+    }
+
+    private void TranslateObstacles(double delta)
+    {
+        foreach (var obstacle in activeObstacles.ToList())
+        {
+            obstacle.Translate(Vector3.Forward * obstacleMoveSpeed * (float)delta);
+            if (!ReachedEnd(obstacle)) continue;
+            FreeObstacle(obstacle);        
+        }
+    }
+
+    private bool ReachedEnd(Node3D node)
+    {
+        return node.Position.Z <= -spawnOffset;
+    }
+
+    private void FreeObstacle(Obstacle obstacle)
+    {
+        RemoveChild(obstacle);
+        obstacle.UnregisterHandlers();
+        activeObstacles.Remove(obstacle);
+        obstacle.Free();
+    }
+}
