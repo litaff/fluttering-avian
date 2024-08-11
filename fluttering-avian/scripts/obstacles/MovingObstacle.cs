c52491f4c225d18a5f1982cc3b933b5aaf27a37f
litaff <a.gazda2k18@gmail.com>
2024-08-11 22:36:36 +0200

    Implemented moving obstacles

---
+++ fluttering-avian/scripts/obstacles/MovingObstacle.cs
@@ -0,0 +1,37 @@
+namespace fluttering_avian.obstacles;
+
+using System;
+using Godot;
+
+public partial class MovingObstacle : Obstacle
+{
+    [Export]
+    private float moveSpeed;
+    
+    private Vector3 direction;
+    private Vector3 offset;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        var random = new Random();
+        direction = random.Next(0, 2) == 0 ? Vector3.Down : Vector3.Up;
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        CalculateOffset(delta);
+        Translate(offset);
+    }
+
+    private void CalculateOffset(double delta)
+    {
+        offset = direction * moveSpeed * (float)delta;
+        
+        if (Position.Y + offset.Y > MinSpawnHeight && Position.Y + offset.Y < MaxSpawnHeight) return;
+
+        direction = -direction;
+        offset = direction * moveSpeed * (float)delta;
+    }
+}
